565c864c758586f8eae3952b3e6eef19839e2e86
GustavoCorona <corona_gustavo@student.smc.com>
2025-07-13 17:40:05 -0700

    Added improvement suggestion comment and new method to playlist.js

--- playlist.js
+++ playlist.js
@@ -43,6 +43,26 @@ Playlist.prototype.skipSong = function() {
   }
 };
 
+// IMPROVEMENT SUGGESTION: Add a way to remove any song by its title would offer more control to managing the playlist in case 
+// you want to alter the genre or overall vibe of the playlist.
+
+// This new method removes a specific song from the playlist by its title.
+Playlist.prototype.removeSongByTitle = function(songTitleToRemove) {
+  // Finds where the song is in the list.
+  const index = this.songs.indexOf(songTitleToRemove);
+  // If the song is found (index is not -1).
+  if (index !== -1) {
+    this.songs.splice(index, 1); // Removes the song from the list.
+    // If the removed song was the one currently playing, update what's playing.
+    if (this.currentSong === songTitleToRemove) {
+      this.currentSong = this.songs.length > 0 ? this.songs[0] : null; // Plays first song or nothing.
+    }
+    return true; // Indicate that the song was removed
+  } else {
+    return false; // Indicate that the song was not found
+  }
+};
+
 // This method displays the object's name and all its items.
 Playlist.prototype.listSongs = function() {
   console.log("Playlist:", this.name);
@@ -57,3 +77,10 @@ myMix.addSong("Evening Jazz");
 myMix.playFirst();
 myMix.skipSong();
 myMix.listSongs();
+
+// Simple Test calls for the new removeSongByTitle method
+console.log("\n--- Testing removeSongByTitle ---");
+myMix.removeSongByTitle("Chillhop Beats"); // It removes a song
+myMix.listSongs(); // List songs to see if it was removed
+myMix.removeSongByTitle("Non-existent Song"); // if it tries to remove a song not there
+myMix.listSongs(); 
